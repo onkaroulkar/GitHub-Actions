0494bd152183fe5991fbf2bc33aba14a2e4f084c
onkar oulkar <onkaroulkar@gmail.com>
2026-01-11 23:05:02 +0530

    first commit

---
+++ src/main/java/com/appsdeveloperblog/estore/service/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.appsdeveloperblog.estore.service;
+
+import com.appsdeveloperblog.estore.model.User;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User createUser(String firstName,
+                           String lastName,
+                           String email,
+                           String password,
+                           String repeatPassword) {
+
+        return new User(firstName, lastName, email);
+
+    }
+}
---
+++ src/test/java/com/appsdeveloperblog/estore/service/UserServiceTest.java
@@ -0,0 +1,34 @@
+package com.appsdeveloperblog.estore.service;
+
+import com.appsdeveloperblog.estore.model.User;
+import com.appsdeveloperblog.estore.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class UserServiceTest {
+
+    @DisplayName("User object created")
+    @Test
+    void testCreateUser_whenUserDetailsProvided_returnsUserObject() {
+        // Arrange
+        UserService userService = new UserServiceImpl();
+        String firstName = "Sergey";
+        String lastName  = "Kargopolov";
+        String email = "test@test.com";
+        String password = "12345678";
+        String repeatPassword = "12345678";
+
+        // Act
+        User user = userService.createUser(firstName, lastName, email, password, repeatPassword);
+
+        // Assert
+        assertNotNull(user, "The createUser() should not have returned null");
+        assertEquals(firstName, user.getFirstName(), "User's first name is incorrect.");
+        assertEquals(lastName, user.getLastName(), "User's last name is incorrect");
+        assertEquals(email, user.getEmail(), "User's email is incorrect");
+    }
+
+}
